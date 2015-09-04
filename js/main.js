56feec0d33637b9f10a631318d1d1c18ba2ca56a
casuneanuandrei <casuneanu.andrei97@gmail.com>
2015-09-04 23:31:30 +0300

    no else

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var stage;
 var wallpaper;
 
 function init(){
+
 	Mousetrap.bind('up', function(){move('up');});
 	Mousetrap.bind('down', function(){move('down');});
 	Mousetrap.bind('right', function(){move('right');});
@@ -12,16 +13,24 @@ function init(){
 	stage.addChild(wallpaper);
 	wallpaper.x = 0;
 	wallpaper.y = 0;
+	wallpaper.velx = 10;
+	wallpaper.vely = 10;
 
 	createjs.Ticker.addEventListener("tick", update);
 }
 
 function move(dir){
 	if (dir == 'up'){
-		wallpaper.y -= 4;
+		wallpaper.y -= wallpaper.vely;
 	}
-	esle if (dir == 'down'){
-		
+	else if (dir == 'down'){
+		wallpaper.y += wallpaper.vely;
+	}
+	else if (dir == 'left'){
+		wallpaper.x -= wallpaper.velx;
+	}
+	else if (dir == 'right'){
+		wallpaper.x += wallpaper.velx;
 	}
 }
 

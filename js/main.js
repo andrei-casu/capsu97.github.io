283ea10498f9e53acd623bbc2c494aaa25e46d7f
casuneanuandrei <casuneanu.andrei97@gmail.com>
2015-09-05 00:10:33 +0300

    square is mooooooooooovin

--- js/main.js
+++ js/main.js
@@ -1,21 +1,65 @@
 var stage;
 var wallpaper;
+var player;
+
+$(document).keydown(function(e) {
+	if (e.which == 37){
+		wallpaper.isMoving = true;
+		wallpaper.direction = "left";
+	}
+	else if (e.which == 38){
+		wallpaper.isMoving = true;
+		wallpaper.direction = "up";
+	}
+	else if (e.which == 39){
+		wallpaper.isMoving = true;
+		wallpaper.direction = "right";
+	}
+	else if (e.which == 40){
+		wallpaper.isMoving = true;
+		wallpaper.direction = "down";
+	}
+	else{
+		wallpaper.isMoving = false;
+	}
+});
+
+$(document).keyup(function(e) {
+	if (e.which >=37 && e.which <=40)
+		wallpaper.isMoving = false;
+});
+
+function moveItem(){
+		if (!this.isMoving)
+			return;
+		if (this.direction == 'left'){
+			this.x -= this.velx;
+		}
+		else if (this.direction == 'up'){
+			this.y -= this.vely;
+		}
+		else if (this.direction == 'right'){
+			this.x += this.velx;
+		}
+		else if (this.direction == 'down'){
+			this.y += this.vely;
+		}
+	};
 
 function init(){
-
-	Mousetrap.bind('up', function(){move('up');});
-	Mousetrap.bind('down', function(){move('down');});
-	Mousetrap.bind('right', function(){move('right');});
-	Mousetrap.bind('left', function(){move('left');});
-
 	stage = new createjs.Stage("canvas");
-	wallpaper = new createjs.Bitmap("assets/wallpaper.png");
-	stage.addChild(wallpaper);
+	wallpaper = new createjs.Bitmap("assets/player.png");
 	wallpaper.x = 0;
 	wallpaper.y = 0;
-	wallpaper.velx = 10;
-	wallpaper.vely = 10;
+	wallpaper.isMoving = false;
+	wallpaper.direction = "none";
+	wallpaper.velx = 7;
+	wallpaper.vely = 7;
+	stage.addChild(wallpaper);
 
+	wallpaper.move = moveItem;
+
+	createjs.Ticker.setFPS(60);
 	createjs.Ticker.addEventListener("tick", update);
 }
 
@@ -35,6 +79,7 @@ function move(dir){
 }
 
 function update(){
-	// wallpaper.x += 5;
+	//wallpaper.x += 5;
+	wallpaper.move();
 	stage.update();
 }

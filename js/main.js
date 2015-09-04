225008e599ee51b1a94bacd1869f8e42a93b9969
casuneanuandrei <casuneanu.andrei97@gmail.com>
2015-09-04 23:21:32 +0300

    A

--- js/main.js
+++ js/main.js
@@ -2,15 +2,30 @@ var stage;
 var wallpaper;
 
 function init(){
+	Mousetrap.bind('up', function(){move('up');});
+	Mousetrap.bind('down', function(){move('down');});
+	Mousetrap.bind('right', function(){move('right');});
+	Mousetrap.bind('left', function(){move('left');});
+
 	stage = new createjs.Stage("canvas");
 	wallpaper = new createjs.Bitmap("assets/wallpaper.png");
 	stage.addChild(wallpaper);
 	wallpaper.x = 0;
+	wallpaper.y = 0;
 
 	createjs.Ticker.addEventListener("tick", update);
 }
 
+function move(dir){
+	if (dir == 'up'){
+		wallpaper.y -= 4;
+	}
+	esle if (dir == 'down'){
+		
+	}
+}
+
 function update(){
-	wallpaper.x += 5;
+	// wallpaper.x += 5;
 	stage.update();
 }

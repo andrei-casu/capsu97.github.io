f64de5d969de0da2247b93fe1aba1ec9d8e60bb3
casuneanuandrei <casuneanu.andrei97@gmail.com>
2015-09-05 02:43:46 +0300

    working without collision at a basic level

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var stage;
-<<<<<<< HEAD
 
 var dif = 1;
 var nrPhantom = 0;
@@ -9,11 +8,11 @@ var phantomSpeed = 7;
 function Phantom(path){
 	createjs.Bitmap.call(this, path);
 
-	var phantomBounds = this.getBounds();
-	this.width = phantomBounds.width;
-	this.height = phantomBounds.height;
-	this.x = parseInt(Math.random()*1000000) % (stage.canvas.height-this.height);
-	this.y = parseInt(Math.random()*1000000) % (stage.canvas.width-this.width);
+	// var phantomBounds = this.getBounds();
+	this.width = 40;//phantomBounds.width;
+	this.height = 40;//phantomBounds.height;
+	this.x = parseInt(Math.random()*1000000) % (stage.canvas.width-this.width);
+	this.y = parseInt(Math.random()*1000000) % (stage.canvas.height-this.height);
 	this.dir = parseInt(Math.random()*100) % 4;
 	//0 up, 1 right, 2 down, 3 left
 
@@ -23,10 +22,10 @@ function Phantom(path){
 	max.x=stage.canvas.width-this.width; 
 	max.y=stage.canvas.height-this.height;
 	this.move = function(dir){
-		if (this.dir == 0 && (this.y - phantomSpeed)==min.y) this.dir=2;
-		else if (this.dir == 2 && (this.y + phantomSpeed)==max.y) this.dir=0;
-		else if (this.dir == 1 && (this.x + phantomSpeed)==max.x) this.dir=3;
-		else if (this.dir == 3 && (this.x - phantomSpeed)==min.x) this.dir=1;
+		if (this.dir == 0 && (this.y - phantomSpeed)<=min.y) this.dir=2;
+		else if (this.dir == 2 && (this.y + phantomSpeed)>=max.y) this.dir=0;
+		else if (this.dir == 1 && (this.x + phantomSpeed)>=max.x) this.dir=3;
+		else if (this.dir == 3 && (this.x - phantomSpeed)<=min.x) this.dir=1;
 
 		if (this.dir == 0) this.y-=phantomSpeed;
 		else if (this.dir == 1) this.x+=phantomSpeed;
@@ -49,9 +48,6 @@ function AddPhantom(){
 	console.log(nrPhantom);
 }
 
-function init(){
-	stage = new createjs.Stage("canvas");
-=======
 var wallpaper;
 var player;
 var keys = [false, false, false, false];
@@ -123,7 +119,6 @@ function init(){
 
 	console.log(player.getBounds());
 	
->>>>>>> 5d4cddf8036f24fdd7177e94d64d637b7f8617ad
 	createjs.Ticker.setFPS(60);
 	createjs.Ticker.addEventListener("tick", update);
 
@@ -131,23 +126,9 @@ function init(){
 }
 
 function update(){
-<<<<<<< HEAD
 	for (var i=0; i<nrPhantom; ++i){
 		phantom[i].move();
 	}
-}
-
-
-
-
-
-
-
-
-
-
-=======
 	player.update();
 	stage.update();
 }
->>>>>>> 5d4cddf8036f24fdd7177e94d64d637b7f8617ad

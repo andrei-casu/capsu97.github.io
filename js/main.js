c6bd779bc5576e93dfebfb851c7a6b38ff04af31
malina-florea <floreamalina@gmail.com>
2015-09-05 02:21:15 +0300

    phantom (bug)

--- js/main.js
+++ js/main.js
@@ -1,85 +1,73 @@
 var stage;
-var wallpaper;
-var player;
 
-$(document).keydown(function(e) {
-	if (e.which == 37){
-		wallpaper.isMoving = true;
-		wallpaper.direction = "left";
-	}
-	else if (e.which == 38){
-		wallpaper.isMoving = true;
-		wallpaper.direction = "up";
-	}
-	else if (e.which == 39){
-		wallpaper.isMoving = true;
-		wallpaper.direction = "right";
-	}
-	else if (e.which == 40){
-		wallpaper.isMoving = true;
-		wallpaper.direction = "down";
-	}
-	else{
-		wallpaper.isMoving = false;
-	}
-});
+var dif = 1;
+var nrPhantom = 0;
+var phantom = [];
+var phantomSpeed = 7;
 
-$(document).keyup(function(e) {
-	if (e.which >=37 && e.which <=40)
-		wallpaper.isMoving = false;
-});
+function Phantom(path){
+	createjs.Bitmap.call(this, path);
 
-function moveItem(){
-		if (!this.isMoving)
-			return;
-		if (this.direction == 'left'){
-			this.x -= this.velx;
-		}
-		else if (this.direction == 'up'){
-			this.y -= this.vely;
-		}
-		else if (this.direction == 'right'){
-			this.x += this.velx;
-		}
-		else if (this.direction == 'down'){
-			this.y += this.vely;
-		}
+	var phantomBounds = this.getBounds();
+	this.width = phantomBounds.width;
+	this.height = phantomBounds.height;
+	this.x = parseInt(Math.random()*1000000) % (stage.canvas.height-this.height);
+	this.y = parseInt(Math.random()*1000000) % (stage.canvas.width-this.width);
+	this.dir = parseInt(Math.random()*100) % 4;
+	//0 up, 1 right, 2 down, 3 left
+
+	var min={}, max={};
+	min.x=0; 
+	min.y=0;
+	max.x=stage.canvas.width-this.width; 
+	max.y=stage.canvas.height-this.height;
+	this.move = function(dir){
+		if (this.dir == 0 && (this.y - phantomSpeed)==min.y) this.dir=2;
+		else if (this.dir == 2 && (this.y + phantomSpeed)==max.y) this.dir=0;
+		else if (this.dir == 1 && (this.x + phantomSpeed)==max.x) this.dir=3;
+		else if (this.dir == 3 && (this.x - phantomSpeed)==min.x) this.dir=1;
+
+		if (this.dir == 0) this.y-=phantomSpeed;
+		else if (this.dir == 1) this.x+=phantomSpeed;
+		else if (this.dir == 2) this.y+=phantomSpeed;
+		else if (this.dir == 3) this.x-=phantomSpeed;
 	};
+}
+Phantom.prototype = Object.create(createjs.Bitmap.prototype);
+Phantom.prototype.constructor = Phantom;
+
+function StartPhantomTimer(){
+	setInterval(AddPhantom, dif*1000);
+}
+
+function AddPhantom(){
+	phantom[nrPhantom] = new Phantom("assets/phantom.png");
+	stage.addChild(phantom[nrPhantom]);
+	++nrPhantom;
+
+	console.log(nrPhantom);
+}
 
 function init(){
 	stage = new createjs.Stage("canvas");
-	wallpaper = new createjs.Bitmap("assets/player.png");
-	wallpaper.x = 0;
-	wallpaper.y = 0;
-	wallpaper.isMoving = false;
-	wallpaper.direction = "none";
-	wallpaper.velx = 7;
-	wallpaper.vely = 7;
-	stage.addChild(wallpaper);
-
-	wallpaper.move = moveItem;
-
 	createjs.Ticker.setFPS(60);
 	createjs.Ticker.addEventListener("tick", update);
-}
 
-function move(dir){
-	if (dir == 'up'){
-		wallpaper.y -= wallpaper.vely;
-	}
-	else if (dir == 'down'){
-		wallpaper.y += wallpaper.vely;
-	}
-	else if (dir == 'left'){
-		wallpaper.x -= wallpaper.velx;
-	}
-	else if (dir == 'right'){
-		wallpaper.x += wallpaper.velx;
-	}
+	StartPhantomTimer();
 }
 
 function update(){
-	//wallpaper.x += 5;
-	wallpaper.move();
-	stage.update();
+	for (var i=0; i<nrPhantom; ++i){
+		phantom[i].move();
+	}
 }
+
+
+
+
+
+
+
+
+
+
